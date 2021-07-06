c0e17542b08fde30fb7415dd12f4d96d7f9032dd
Ernest Walzel <ewalzel@gmail.com>
2021-07-06 19:04:54 +0200

    First component (Button)

---
+++ src/components/Button.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Button({ label, onClick }) {
+  return (
+    <button
+      class="govuk-button js-uc-submit govuk-button--start"
+      type="submit"
+      onClick={onClick}
+    >
+      <span>{label}</span>
+      <svg
+        class="govuk-button__start-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="17.5"
+        height="19"
+        viewBox="0 0 33 40"
+        focusable="false"
+      >
+        <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z"></path>
+      </svg>
+    </button>
+  );
+}
+
+Button.propTypes = {
+  label: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default Button;
