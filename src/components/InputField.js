b052228334a73ce15e9b66e587f184f1ef0733ef
Ernest Walzel <ewalzel@gmail.com>
2021-07-07 05:37:15 +0200

    Tidy up classes in InputField

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,31 +1,33 @@
 import React from "react";
-
-import { ErrorMessage, Field, useFormikContext } from "formik";
+import { Field, useFormikContext } from "formik";
+import cn from "classnames";
+import ErrorMessage from "components/ErrorMessage";
 
 function InputField(props) {
-    const { errors, touched } = useFormikContext();
+  const { errors, touched } = useFormikContext();
+  const hasError = errors[props.name] && touched[props.name];
+  const groupClassName = cn("govuk-form-group govuk-!-margin-bottom-3", {
+    "govuk-form-group--error": hasError,
+  });
+  const fieldClassName = cn("govuk-input govuk-input--width-20", {
+    "govuk-input--error": hasError,
+  });
 
-    function errorClassName(name, className) {
-        return errors[name] && touched[name] ? className : '';
-    }
-
-    return (
-        <div className={`govuk-form-group govuk-!-margin-bottom-3 ${errorClassName(props.name, 'govuk-form-group--error')}`}>
-            <label className="govuk-label" htmlFor={props.name}>
-                <strong>{props.label}</strong>
-            </label>
-            <div className="govuk-hint">{props.hint}</div>
-            <ErrorMessage name={props.name}>
-                {(msg) => <span className="govuk-error-message">{msg}</span>}
-            </ErrorMessage>
-            <Field
-                id={props.name}
-                name={props.name}
-                type={props.type || "text"}
-                className={`govuk-input govuk-input--width-20 ${errorClassName(props.name, 'govuk-input--error')}`}
-            />
-        </div>
-    );
+  return (
+    <div className={groupClassName}>
+      <label className="govuk-label" htmlFor={props.name}>
+        <strong>{props.label}</strong>
+      </label>
+      <div className="govuk-hint">{props.hint}</div>
+      <ErrorMessage name={props.name} />
+      <Field
+        id={props.name}
+        name={props.name}
+        type={props.type || "text"}
+        className={fieldClassName}
+      />
+    </div>
+  );
 }
 
 export default InputField;
