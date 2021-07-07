5f3e512057b89372b5c29acdba21117f21f45e36
Ernest Walzel <ewalzel@gmail.com>
2021-07-07 18:11:55 +0200

    Lint Fieldset

--- src/components/Fieldset.js
+++ src/components/Fieldset.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 function Fieldset(props) {
-
   return (
     <fieldset className="govuk-fieldset govuk-!-margin-bottom-5" role="group">
-      {props.legend && <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">
-        <h2 className="govuk-fieldset__heading">{props.legend}</h2>
-      </legend>}
+      {props.legend && (
+        <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">
+          <h2 className="govuk-fieldset__heading">{props.legend}</h2>
+        </legend>
+      )}
 
       {props.children}
     </fieldset>
